1caeb9ef1231524d098a47f809152f792cbbac16
steugen <p.gnomme@gmail.com>
2024-04-07 16:17:18 +0400

    playbook's ready

--- my-fast-ubuntu/scripts/neovim_08_build.sh
+++ my-fast-ubuntu/scripts/neovim_08_build.sh
@@ -9,3 +9,5 @@ git clone https://github.com/neovim/neovim ~/
 cd ~/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 
 cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim/
